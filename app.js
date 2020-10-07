3be5a0abeadbca212027afe379d9859be3191cfe
andreocunha <andreoliveiracunha20@gmail.com>
2020-10-07 18:19:40 -0300

    Integracao com socket.io

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const http = require('http')
+
+const server = http.createServer(app) // esse server é quem o socket.io vai ficar ouvindo
+
+const socketIo = require('socket.io')
+
+const io = socketIo.listen(server)
+
+server.listen(3000,() => {
+    console.log("running")
+})
+
+app.use(express.static(__dirname + "/public"))
+
+const historico = []
+
+// Esse é "io" é responsável por ficar checando se alguma coisa tá acontecendo (no caso aí é a conexão)
+io.on('connection', (socket) => {
+    console.log("Nova conexão")
+
+    historico.forEach(linha => {
+        socket.emit('desenhar', linha) // o "socket" manda só para o que acabou de fazer a conexao
+    })
+
+    socket.on('desenhar', (linha)=>{
+        historico.push(linha)
+        io.emit('desenhar', linha) // o "io" manda para todos
+    })
+
+    socket.on('limpar', ()=>{
+        for (var i = historico.length; i > 0; i--) {
+            historico.pop();
+        }
+        io.emit('limpar')
+    })
+})
